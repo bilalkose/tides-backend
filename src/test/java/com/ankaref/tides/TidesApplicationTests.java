a75a1eac933906d10085554cd04674290c3144e7
Bilal Muzaffer Kose <bilalmuzafferkose@gmail.com>
2022-08-06 12:05:51 +0300

    first commit

---
+++ src/test/java/com/ankaref/tides/TidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankaref.tides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
